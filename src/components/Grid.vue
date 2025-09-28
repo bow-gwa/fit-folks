cc10f11fa363d394da8e2e5b1f1a6a810cb4d518
bow-gwa <bowgwa.bowgwa@gmail.com>
2025-09-28 15:55:45 +0300

    Added grid

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,8 +1,21 @@
 <script setup>
+import { workoutProgram } from '../utils';
+console.log(workoutProgram)
 </script>
 
 <template>
-    <p> Grid</p>
+<section id="grid">
+    <button :key="workoutIdx" v-for="(workout, workoutIdx) in Object.keys(workoutProgram)" class="card-button plan-card">
+        <div>
+            <p>Day {{ workoutIdx < 9 ? "0" + (workoutIdx + 1) : workoutIdx + 1 }}</p>
+            <i class="fa-solid fa-dumbbell" v-if="workoutIdx % 3 == 0"></i>
+            <i class="fa-solid fa-weight-hanging" v-if="workoutIdx % 3 == 1"></i>
+            <i class="fa-solid fa-bolt" v-if="workoutIdx % 3 == 2"></i>
+            
+        </div>
+        <h3>{{ workoutTypes[workoutIdx % 3] }}</h3>
+    </button>
+</section>
 </template>
 
 <style scoped>
