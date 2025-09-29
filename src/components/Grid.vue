e7def185f550fc6aaca6e8bdc6abff7bd0c2c7ec
bow-gwa <bowgwa.bowgwa@gmail.com>
2025-09-29 19:13:55 +0300

    Normalize line endings

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { workoutProgram } from '../utils';
-console.log(workoutProgram)
+
+const workoutTypes = ["Push", "Pull", "Legs"]
 </script>
 
 <template>
@@ -11,13 +12,43 @@ console.log(workoutProgram)
             <i class="fa-solid fa-dumbbell" v-if="workoutIdx % 3 == 0"></i>
             <i class="fa-solid fa-weight-hanging" v-if="workoutIdx % 3 == 1"></i>
             <i class="fa-solid fa-bolt" v-if="workoutIdx % 3 == 2"></i>
-            
         </div>
+        
         <h3>{{ workoutTypes[workoutIdx % 3] }}</h3>
     </button>
 </section>
 </template>
 
 <style scoped>
+#grid {
+    display: grid;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    gap: 1rem;
+}
 
+#grid button {
+    width: 100%
+}
+
+.plan-card {
+    display: flex;
+    flex-direction: column;
+}
+
+.plan-card div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 0.5rem;
+}
+
+.plan-card div p {
+    text-align: left;
+}
+
+@media (min-width: 640px) {
+    #grid {
+        grid-template-colums: repeat(4, minmax(0, 1fr));
+    }
+}
 </style>
